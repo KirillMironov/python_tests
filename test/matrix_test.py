962023008f4f9c459d16f7d8a6461cec615f5494
Kirill <kirill@localhost>
2018-10-13 19:41:15 +0300

    Unittest

--- test/matrix_test.py
+++ test/matrix_test.py
@@ -5,8 +5,10 @@ import numpy as np
 class MatrixTest(unittest.TestCase):
 
     def test_matrix(self):
-        matrix = np.array([[1, 2], [2, 3]])
-        np.testing.assert_array_equal(matrix, [[1, 2], [2, 3]])
+        matrix1 = np.array([[-1, 1], [2, 0], [0, 3]])
+        matrix2 = np.array([[3, 1, 2], [0, -1, 4]])
+        result = matrix1 @ matrix2
+        np.testing.assert_array_equal(result, [[-3, -2, 2], [6, 2, 4], [0, -3, 12]])
 
 
 if __name__ == '__main__':
